5bed5d97aaa26b3e753399f41f158ae4a13edd5f
ananya-bangera <ananya.bangera100@gmail.com>
2024-05-09 23:16:34 +0530

    Updated Trades Page with create POV form

--- talevault/packages/nextjs/app/tales/_components/MarkDown.tsx
+++ talevault/packages/nextjs/app/tales/_components/MarkDown.tsx
@@ -1,19 +1,21 @@
 "use client";
 import React from "react";
 import MDEditor from '@uiw/react-md-editor';
-
+// import rehypeSanitize from "rehype-sanitize";
+import MarkdownPreview from '@uiw/react-markdown-preview';
 export default function MarkDown() {
   const [value, setValue] = React.useState("**Hello world!!!**");
   return (
     <div className="container">
       <MDEditor
+       className="rounded-full"
         value={value}
-        // height={100}
-
-        // minHeight={50}
         visibleDragbar={false}
         onChange={setValue}
+        
+        
       />
+        {/* <MDEditor.Markdown source={value} /> */}
     </div>
   );
 }
