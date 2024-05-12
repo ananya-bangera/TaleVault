9d4d493710c9f4e94cf73a0d097582b93baf9f19
ananya-bangera <ananya.bangera100@gmail.com>
2024-05-12 23:18:16 +0530

    Updated Lens Client

--- talevault/packages/nextjs/app/lens/page.tsx
+++ talevault/packages/nextjs/app/lens/page.tsx
@@ -1,30 +1,84 @@
 "use client";
 
 import React from "react";
-import { LensClient, development } from "@lens-protocol/client";
+import { LensClient, Wallet, development, isRelaySuccess } from "@lens-protocol/client";
 import { useAccount } from "wagmi";
+
 
 export default function Test() {
   const account = useAccount();
-  async function getAuthenticatedClientFromEthersWallet(): Promise<LensClient> {
+  async function getProfile() {
+    var handle = "ananya";
     const lensClient = new LensClient({
       environment: development,
+      
+      headers: {
+        'user-agent': "spectaql",
+        // userAgent: "spectaql",
+      //   "Access-Control-Allow-Credentials": "true",
+      //   'Access-Control-Allow-Origin': '*'
+      },
     });
-
-    const { id, text } = await lensClient.authentication.generateChallenge({
-      signedBy: account.address ?? "",
-      for: "Ananya",
+    const profileCreateResult = await lensClient.wallet.createProfileWithHandle({
+      handle: handle,
+      to: account.address?.toString() || "",
     });
-    const signature = await wallet.signMessage(text);
+    // console.log(profileCreateResult);
+    // const profileCreateResultValue = profileCreateResult.unwrap();
 
-    await lensClient.authentication.authenticate({ id, signature });
-    console.log(lensClient.wallet);
-    console.log(lensClient);
-    return lensClient;
+    // if (!isRelaySuccess(profileCreateResultValue)) {
+    //   console.log(`Something went wrong`, profileCreateResultValue);
+    //   return;
+    // }
+
+    // console.log(
+    //   `Transaction to create a new profile with handle "${handle}" was successfuly broadcasted with txId ${profileCreateResultValue.txId}`,
+    // );
+
+    // console.log(`Waiting for the transaction to be indexed...`);
+    // await lensClient.transaction.waitUntilComplete({
+    //   txId: profileCreateResultValue.txId,
+    // });
+
+    // const allOwnedProfiles = await lensClient.profile.fetchAll({
+    //   where: {
+    //     ownedBy: [account.address?.toString() || ""],
+    //   },
+    // });
+
+    // console.log(
+    //   `All owned profiles: `,
+    //   allOwnedProfiles.items.map(i => ({ id: i.id, handle: i.handle })),
+    // );
+
+    // const newProfile = allOwnedProfiles.items.find(item => item.handle === `${handle}.test`);
+
+    // if (newProfile) {
+    //   console.log(`The newly created profile's id is: ${newProfile.id}`);
+    // }
   }
+  //  async function getAuthenticatedClientFromEthersWallet(wallet: Wallet): Promise<LensClient> {
+
+  //   const lensClient = new LensClient({
+  //     environment: development,
+  //   });
+
+  //   const address = await wallet.getAddress();
+
+  //   const { id, text } = await lensClient.authentication.generateChallenge({
+  //     signedBy: address,
+  //     for: "",
+  //   });
+  //   const signature = await wallet.signMessage(text);
+
+  //   await lensClient.authentication.authenticate({ id, signature });
+  //   console.log(lensClient);
+
+  //   return lensClient;
+  // }
   return (
     <div>
-      <button onClick={() => getAuthenticatedClientFromEthersWallet()}>Check</button>
+      <button onClick={() => getProfile()}>Check</button>
     </div>
   );
 }
