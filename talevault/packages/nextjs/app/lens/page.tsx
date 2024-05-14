12a0b9d7d8b2ba6a9d85db7675c5fa6e968165ee
ananya-bangera <ananya.bangera100@gmail.com>
2024-05-15 01:48:36 +0530

    Create Publications Lens Protocol

--- talevault/packages/nextjs/app/lens/page.tsx
+++ talevault/packages/nextjs/app/lens/page.tsx
@@ -2,16 +2,23 @@
 
 import React from "react";
 import { ChangeProfileManagerActionType, LensClient, Wallet, development, isRelaySuccess } from "@lens-protocol/client";
+import { LinkMetadata, MediaImageMimeType, image, link } from "@lens-protocol/metadata";
 import { ethers } from "ethers";
+import { encode } from "punycode";
+import { v4 as uuidv4 } from "uuid";
 import { useAccount } from "wagmi";
+import { setEnvironmentData } from "worker_threads";
 
 export default function Test() {
   const account = useAccount();
   let handle = "";
-  let profile_id = "0x0203";
+  let profile_id = "0x0202";
   const lensClient = new LensClient({
     environment: development,
   });
+
+  let pinataApiKey = process.env.NEXT_PUBLIC_PINATA_API_KEY;
+  let pinataApiSecret = process.env.NEXT_PUBLIC_PINATA_API_SECRET;
   async function getProfile() {
     handle = "ananya" + Date.now().toString();
 
@@ -110,6 +117,7 @@ export default function Test() {
       `Successfully changed profile managers with transaction with id ${onchainRelayResult}, txHash: ${onchainRelayResult.txHash}`,
     );
   }
+
   async function followProfileID() {
     const isAuthenticated = await lensClient.authentication.isAuthenticated();
     console.log(isAuthenticated);
@@ -138,40 +146,80 @@ export default function Test() {
       );
     }
   }
+  async function pinMetadataToPinata(metadata: any, contentName: any, pinataApiKey: any, pinataApiSecret: any) {
+    console.log("pinning metadata to pinata...");
+    const data = JSON.stringify({
+      pinataMetadata: { name: contentName },
+      pinataContent: metadata,
+    });
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        pinata_api_key: pinataApiKey,
+        pinata_secret_api_key: pinataApiSecret,
+      },
+      body: data,
+    };
+    const response = await fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS", config);
+    const ipfsHash = (await response.json()).IpfsHash;
+    console.log(`Stored content metadata with ${ipfsHash}`);
+    return ipfsHash;
+  }
+  const createContentMetadata = function (content: any, contentName: any, imageUri: any, imageType: any) {
+    return {
+      version: "2.0.0",
+      metadata_id: uuidv4(),
+      description: "Created from LensBlog",
+      content: content,
+      name: contentName,
+      mainContentFocus: "ARTICLE",
+      attributes: [],
+      locale: "en-US",
+      appId: "lensblog",
+      image: imageUri,
+      imageMimeType: imageType,
+      tags:["tag1","tag2","tag3"]
+    };
+  };
+  async function getContentURI(content: any, contentName: any, imageUri: any, imageType: any){
+    let fullContentURI ="";
+
+    const contentMetadata = createContentMetadata(content, contentName, imageUri, imageType);
+
+
+    const BASE_64_PREFIX = "data:application/json;base64,";
+    if (pinataApiSecret && pinataApiKey) {
+      const metadataIpfsHash = await pinMetadataToPinata(contentMetadata, contentName, pinataApiKey, pinataApiSecret);
+      fullContentURI = `ipfs://${metadataIpfsHash}`;
+      console.log(fullContentURI);
+    } else {
+      const base64EncodedContent = encode(JSON.stringify(contentMetadata));
+      const fullContentURI = BASE_64_PREFIX + base64EncodedContent;
+    }
+    return fullContentURI;
+  }
   async function createPost() {
     const isAuthenticated = await lensClient.authentication.isAuthenticated();
     console.log(isAuthenticated);
     if (isAuthenticated) {
-      // const result = await lensClient.publication.postOnchain({
-      //   contentURI: "ipfs://QmZ1A9npSQvV72nYy9UJb2K42J4U7P9HXM1moW6R2L5sLG", // or arweave
-      // });
-      const resultTypedData = await lensClient.publication.createOnchainPostTypedData({
-        contentURI: 'ipfs://QmZ1A9npSQvV72nYy9UJb2K42J4U7P9HXM1moW6R2L5sLG', // or arweave
+      var content = "content Ananya";
+      var contentName = "contentName Ananya";
+      var imageUri = "ipfs://QmZ1A9npSQvV72nYy9UJb2K42J4U7P9HXM1moW6R2L5sLG";
+      var imageType = "image/png";
+      var fullContentURI = await getContentURI(content, contentName, imageUri, imageType);  
+
+      const result = await lensClient.publication.postOnchain({
+        contentURI: fullContentURI, // or arweave
       });
-      
-      const { id, typedData } = resultTypedData.unwrap();
+      console.log(result);
+      const resultValue = result.unwrap();
 
-      // sign with the wallet
-      // sign with the wallet
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const signedTypedData = await signer._signTypedData(typedData.domain, typedData.types, typedData.value);
-
-      console.log(`Broadcasting signed typed data...`);
-
-      const broadcastResult = await lensClient.transaction.broadcastOnchain({
-        id,
-        signature: signedTypedData,
-      });
-
-      const broadcastValue = broadcastResult.unwrap();
-
-      if (!isRelaySuccess(broadcastValue)) {
-        console.log(`Something went wrong`, broadcastValue);
+      if (!isRelaySuccess(resultValue)) {
+        console.log(`Something went wrong`, resultValue);
         return;
       }
-
-      console.log(`Transaction was successfully broadcasted with txId ${broadcastValue.txId}`);
+      console.log(`Transaction was successfully broadcasted with txId ${resultValue.txId}`);
     }
   }
   async function checkPublications() {
@@ -179,15 +227,86 @@ export default function Test() {
     console.log(isAuthenticated);
     if (isAuthenticated) {
       // lensClient.publication.fetchAll
-      const result = await lensClient.publication.fetch({
-        forId: '0x0203'//'9300e17e-987f-4df2-b0c9-519609f6710c'
-        // forTxHash:'0x3b491c8969390b6f097a5440993b4c52c484fd2baeab887feb4038b0181b682c'
-        //forId: 'ca91c8f1-4b0f-4ff6-8774-5a95c93a4551', //"0x78ba2fbd57eb339719d0af173b7709d93b9125d2d5910cf71e0ad4505901a5a9"
+      const result = await lensClient.publication.fetchAll({
+        // forId: '0x0203'//'9300e17e-987f-4df2-b0c9-519609f6710c'
+        // forTxHash: "0xa552309a7f75258aeab723d441d0e5bc771eef2dcc3e0e9ce802c3b3d942dabc",
+        // forTxHash: "0xdf09971dd9941ab3fcb83293dc9afb41f5157ff7610dc9a9eca3ae8939f6e421",
+        // forId: 'ca91c8f1-4b0f-4ff6-8774-5a95c93a4551', //"0x78ba2fbd57eb339719d0af173b7709d93b9125d2d5910cf71e0ad4505901a5a9"
+        where: {
+          from: ["0x0202"],
+        },
       });
       console.log(result);
     }
   }
+  async function checkstatus() {
+    const result = await lensClient.transaction.status({ forTxId: data });
 
+    // or wait till status is complete
+    // const result = await lensClient.transaction.waitUntilComplete({
+    //   forTxId: TX_ID,
+    // });
+    console.log(result);
+  }
+  async function createMetadata() {
+    // const metadata = link({
+    // sharingLink:"ipfs://QmZ1A9npSQvV72nYy9UJb2K42J4U7P9HXM1moW6R2L5sLG"
+    // })
+    //0x0202-0x0d
+    const metadata = image({
+      image: {
+        item: "ipfs://QmZ1A9npSQvV72nYy9UJb2K42J4U7P9HXM1moW6R2L5sLG",
+        type: MediaImageMimeType.PNG,
+      },
+    });
+    const result = await lensClient.publication.validateMetadata({
+      json: JSON.stringify(metadata),
+    });
+    console.log(result);
+  }
+  async function addComment() {
+    // const metadata = link({
+    // sharingLink:"ipfs://QmZ1A9npSQvV72nYy9UJb2K42J4U7P9HXM1moW6R2L5sLG"
+    // })
+    //0x0202-0x0d
+
+    const isAuthenticated = await lensClient.authentication.isAuthenticated();
+    console.log(isAuthenticated);
+    if (isAuthenticated) {
+      var content = "COMMENT on Post";
+      var contentName = "Comment Message";
+      var imageUri = "";
+      var imageType = "";
+      var fullContentURI = await getContentURI(content, contentName, imageUri, imageType);  
+
+      const result = await lensClient.publication.commentOnchain({
+        commentOn: '0x0202-0x0d',
+        contentURI: fullContentURI, // or arweave
+      });
+      
+      console.log(result);
+      const resultValue = result.unwrap();
+      //d4727e1e-4abb-4ade-842e-4eac1391a9ef
+      if (!isRelaySuccess(resultValue)) {
+        console.log(`Something went wrong`, resultValue);
+        return;
+      }
+      
+      console.log(`Transaction was successfully broadcasted with txId ${resultValue.txId}`);
+    }
+   }
+   async function createMirror(){
+    const result = await lensClient.publication.mirrorOnchain({
+      mirrorOn: '0x0202-0x0d',
+    });
+    console.log(result);
+   }
+  async function getTags() {
+
+    const result = await lensClient.publication.tags();
+    console.log(result);
+  }
+ const [data,setData]=React.useState("")
   return (
     <div>
       <button className="btn" onClick={() => getProfile()}>
@@ -220,8 +339,24 @@ export default function Test() {
       <button className="btn" onClick={() => createPost()}>
         createPost
       </button>
+      <input onChange={(e)=>{setData(e.target.value)}}></input>
       <button className="btn" onClick={() => checkPublications()}>
         checkPublications
+      </button>
+      <button className="btn" onClick={() => checkstatus()}>
+        checkstatus
+      </button>
+      <button className="btn" onClick={() => createMetadata()}>
+        createMetadata
+      </button>
+      <button className="btn" onClick={() => getTags()}>
+      getTags
+      </button>
+      <button className="btn" onClick={() => addComment()}>
+      addComment
+      </button>
+      <button className="btn" onClick={() => createMirror()}>
+      createMirror
       </button>
     </div>
   );
